91063e49e3f67272c49d0350fb7519496f754b3b
Euxcbsks <hawhaw02030746@gmail.com>
2023-02-24 13:14:59 +0800

    fix: review

--- src/bot/extensions/logger.py
+++ src/bot/extensions/logger.py
@@ -1,7 +1,6 @@
-from datetime import datetime, timezone, timedelta
+from datetime import datetime, timedelta, timezone
 
-from discord import Message, Embed, Member, Color
-
+from discord import Color, Embed, Message, TextChannel
 from packages.cog_data import *
 
 
@@ -9,58 +8,52 @@ class LoggerCog(discord.Cog):
     def __init__(self, bot: Bot) -> None:
         super().__init__()
         self.bot: Bot = bot
-        self.event_config = Config({
-            "msg": {
-                "channel": self.bot.get_channel(832849374395760660)
-            }
-        })
-        self.embed_config = Config({
-            "delete": {
-                "name": "刪除",
-                "color": Color.red()
-            },
-            "edit": {
-                "name": "修改",
-                "color": Color.green()
-            }
-        })
-    
-    async def send_msg_log_embed(self, type: str, msgs: list[Message]) -> Message:
-        data = self.embed_config[type]
-        embed = Embed(f'訊息{data.name}紀錄', 
-                      description = f'{msgs[0].author.mention} 在 {msgs[0].channel.mention} {data.name}訊息',
-                      color=data.color)
-        embed.set_footer(datetime.now(tz=timezone(timedelta(hours=8))).strftime(r'%A'))
-        match type:
-            case 'delete':
-                embed.add_field(name = f'{data.name}的訊息內文',
-                                value = msgs[0].content)
-            case 'edit':
-                embed.add_field(name = '原始的訊息內文',
-                                value = msgs[0].content)
-                embed.add_field(name = f'{data.name}的訊息內文',
-                                value = msgs[1].content)
-        return await self.event_config.msg.send(embed=embed)
-                
-    
+        self.event_config = {"msg": {"channel": self.bot.get_channel(832849374395760660)}}
+        self.embed_config = {
+            "delete": {"name": "刪除", "color": Color.red()},
+            "edit": {"name": "修改", "color": Color.green()},
+        }
+
+    def embed_gen(self, type_: str, *msgs: Message):
+        translated_type, color = self.embed_config[type_].values()
+        msg, after = msgs
+        embed = Embed(
+            title=f"訊息{translated_type}紀錄",
+            description=f"{msg.author.mention}在{msg.channel.mention}{translated_type}了訊息",  # type: ignore
+            color=color,
+        ).set_footer(text=datetime.now(tz=timezone(timedelta(hours=8))).strftime(r"%A"))
+
+        match type_:
+            case "delete":
+                embed.add_field(name=f"{translated_type}的訊息內文", value=msg.content)
+            case "edit":
+                embed.add_field(name="原始的訊息內文", value=msg.content).add_field(
+                    name=f"{translated_type}的訊息內文", value=after.content
+                )
+
+        return embed
+
     @Cog.listener()
     async def on_message_delete(self, msg: Message):
-        await self.send_msg_log_embed('delete', [msg])
-        logging.info(f'{msg.author} delete message:\n'
-                     f'\t{msg.content}')
-    
+        channel: TextChannel = self.event_config["msg"]["channel"]  # type: ignore
+        await channel.send(embed=self.embed_gen("delete", msg))
+        logging.info(f"{msg.author} delete message:\n" f"\t{msg.content}")
+
     @Cog.listener()
     async def on_message_edit(self, before_msg: Message, after_msg: Message):
-        await self.send_msg_log_embed('edit', [before_msg, after_msg])
-        logging.info(f'{before_msg.author}\'s message have deleted\n'
-                      'origin message:\n'
-                     f'\t{before_msg.content}\n'
-                      'edited message:\n'
-                     f'\t{after_msg.content}')
-    
+        channel: TextChannel = self.event_config["msg"]["channel"]  # type: ignore
+        await channel.send(embed=self.embed_gen("edit", before_msg, after_msg))
+        logging.info(
+            f"{before_msg.author}'s message have deleted\n"
+            "origin message:\n"
+            f"\t{before_msg.content}\n"
+            "edited message:\n"
+            f"\t{after_msg.content}"
+        )
+
     @Cog.listener()
     async def on_application_command(self, ctx: ApplicationContext):
-        logging.info(f'{ctx.author} used {ctx.command.name}')
+        logging.info(f"{ctx.author} used {ctx.command.name}")
 
 
 def setup(bot: Bot):
--- src/bot/packages/default_data.py
+++ src/bot/packages/default_data.py
@@ -1,31 +1,38 @@
 import logging
 
 import discord
-from discord import Bot, commands, ApplicationContext
+from default_data import commands_checks, option
+from discord import ApplicationContext, Bot, commands
+from discord.ext.commands import check
 
-class Option:
-    default = {
-        "guild_ids": [
-            815819580840607807
-        ]
-    }
 
-option = Option()
+class BotOption:
+    default = {"guild_ids": [815819580840607807]}
+
+
+option = BotOption()
+
 
 class Config:
     def __init__(self, data: dict) -> None:
         for key, value in data.items():
             if isinstance(value, dict):
-                setattr(key, Config(value))
+                setattr(self, key, Config(value))
             else:
-                setattr(key, value)
-    
+                setattr(self, key, value)
+
     def __getitem__(self, key: str):
         return getattr(self, key)
 
+
 class commands_checks:
-    def is_developer(ctx: ApplicationContext):
-        return ctx.author.id in {
-            467532880625664000, # YT Mango#4092
-            645588343228334080  # 菘菘#8663
-        }
+    @staticmethod
+    def is_developer():
+        def predicate(ctx):
+            return ctx.author.id in {
+                467532880625664000,  # YT Mango#4092
+                645588343228334080,  # 菘菘#8663
+                577086242806693898,  # Euxcbsks#5316
+            }
+
+        return check(predicate)

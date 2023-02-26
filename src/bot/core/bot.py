b80ea78bc040b673c7b563019ba28991d42060c6
Mango <evan940415@gmail.com>
2023-02-26 10:29:58 +0800

    add: bot config into attribute

--- src/bot/core/bot.py
+++ src/bot/core/bot.py
@@ -1,13 +1,20 @@
 import logging
+import tomllib
+import os
 
 from discord import Bot, Intents
 
+from packages.default_data import Config
 
 class RPMBot(Bot):
     def __init__(self):
-        intents = Intents.default()
-        intents.message_content = True
         super().__init__(intents=Intents.all())
+        
+        _config = {}
+        for filename in os.listdir('./src/bot/data'):
+            with open(f'./src/bot/data/{filename}', 'rb') as file:
+                _config[filename.removesuffix('.toml')] = tomllib.load(file)
+        self.config = Config(_config)
 
     async def on_ready(self):
         logging.info("bot is ready")

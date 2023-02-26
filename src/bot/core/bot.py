f39a4588976ac25723eb8653df0f297c84112941
Euxcbsks <hawhaw02030746@gmail.com>
2023-02-26 09:09:18 +0800

    Fix: turn off unused intents

--- src/bot/core/bot.py
+++ src/bot/core/bot.py
@@ -5,6 +5,8 @@ from discord import Bot, Intents
 
 class RPMBot(Bot):
     def __init__(self):
+        intents = Intents.default()
+        intents.message_content = True
         super().__init__(intents=Intents.all())
 
     async def on_ready(self):

c678df843ba0570f7a89e924f2d13e449399cc35
Euxcbsks <hawhaw02030746@gmail.com>
2023-02-26 11:27:27 +0800

    fix: turn off unused intents

--- src/bot/core/bot.py
+++ src/bot/core/bot.py
@@ -1,20 +1,22 @@
 import logging
-import tomllib
-import os
+from os import listdir
 
 from discord import Bot, Intents
-
 from packages.default_data import Config
+from tomllib import load
+
 
 class RPMBot(Bot):
     def __init__(self):
-        super().__init__(intents=Intents.all())
-        
-        _config = {}
-        for filename in os.listdir('./src/bot/data'):
-            with open(f'./src/bot/data/{filename}', 'rb') as file:
-                _config[filename.removesuffix('.toml')] = tomllib.load(file)
-        self.config = Config(_config)
+        intents = Intents.default()
+        intents.message_content = True
+        super().__init__(intents=intents)
+
+        data = {}
+        for filename in listdir("./src/bot/data"):
+            with open(f"./src/bot/data/{filename}", "rb") as file:
+                data[filename.removesuffix(".toml")] = load(file)
+        self.config = Config(data)
 
     async def on_ready(self):
         logging.info("bot is ready")

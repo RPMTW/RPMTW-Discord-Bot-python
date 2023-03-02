859c61aaa46d54e6e2cfb1250d3b6486b8c0b8aa
Ray <56812006+Euxcbsks@users.noreply.github.com>
2023-03-02 23:55:34 +0000

    feat: implement dynamic voice

--- src/bot/extensions/dynamic_voive.py
+++ src/bot/extensions/dynamic_voive.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING
 
-from discord import Member, VoiceChannel, VoiceState
+from discord import Member, VoiceChannel, VoiceState, PermissionOverwrite
 from packages.cog_data import *
 
 if TYPE_CHECKING:
@@ -24,18 +24,24 @@ class DynamicVoiceCog(InitedCog):
     async def on_voice_state_update(
         self, member: Member, before: VoiceState, after: VoiceState
     ):
-        await self.ensure_exist()
-        """
-        #? join
-        1. a join main_channel
-        2. create new_channel
-        3. move a into new_channel
+        if member.bot:
+            return
 
-        #? leave
-        1. a leave new_channel
-        2. delete new_channel
-        """
-        ...
+        await self.ensure_exist()
+
+        # leave/move from exclusive channel
+        if (channel := self.voice_list.get(member.id)) and (_ := before.channel) and channel.id == _.id:
+            del self.voice_list[member.id]
+            await channel.delete()
+
+        # join/move to main_channel
+        if (channel := after.channel) and channel.id == self.main_channel.id:
+            overwrites = {
+                member: PermissionOverwrite(manage_roles=True, manage_channels=True),
+                member.guild.default_role: PermissionOverwrite(priority_speaker=True),
+                self.bot.user: PermissionOverwrite(priority_speaker=False)
+            }
+            self.voice_list[member.id] = await channel.category.create_voice_channel(f"{member.name}的頻道", overwrites=overwrites)
 
 
 def setup(bot: "RPMBot"):

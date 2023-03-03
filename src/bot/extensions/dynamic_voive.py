271a5e0449980568fb066981e03d41db5702ca8d
EvanHsieh0415 <evan940415@gmail.com>
2023-03-03 09:32:19 +0800

    fix: move_to & clear empty & logging

--- src/bot/extensions/dynamic_voive.py
+++ src/bot/extensions/dynamic_voive.py
@@ -1,6 +1,12 @@
 from typing import TYPE_CHECKING
 
-from discord import Member, VoiceChannel, VoiceState, PermissionOverwrite
+from discord import (
+    Member,
+    VoiceChannel,
+    VoiceState,
+    PermissionOverwrite,
+    CategoryChannel,
+)
 from packages.cog_data import *
 
 if TYPE_CHECKING:
@@ -11,7 +17,7 @@ class DynamicVoiceCog(InitedCog):
     def __init__(self, bot: "RPMBot") -> None:
         super().__init__(bot)
 
-        self.voice_list: dict[str, int] = {}
+        self.voice_mapping: dict[int, VoiceChannel] = {}
         self.config = self.bot.config["constant.dynamic.voice"]
         self.main_channel: VoiceChannel = None  # type: ignore
 
@@ -30,18 +36,45 @@ class DynamicVoiceCog(InitedCog):
         await self.ensure_exist()
 
         # leave/move from exclusive channel
-        if (channel := self.voice_list.get(member.id)) and (_ := before.channel) and channel.id == _.id:
-            del self.voice_list[member.id]
+        if (
+            (channel := self.voice_mapping.get(member.id))
+            and (_ := before.channel)
+            and channel.id == _.id
+        ):
+            del self.voice_mapping[member.id]
             await channel.delete()
+            logging.info(f"{member} leave his/her exclusive channel(id={channel.id})")
 
         # join/move to main_channel
-        if (channel := after.channel) and channel.id == self.main_channel.id:
+        if (main_channel := after.channel) and channel.id == self.main_channel.id:
             overwrites = {
                 member: PermissionOverwrite(manage_roles=True, manage_channels=True),
                 member.guild.default_role: PermissionOverwrite(priority_speaker=True),
-                self.bot.user: PermissionOverwrite(priority_speaker=False)
+                self.bot.user: PermissionOverwrite(priority_speaker=False),
             }
-            self.voice_list[member.id] = await channel.category.create_voice_channel(f"{member.name}的頻道", overwrites=overwrites)
+            self.voice_mapping[
+                member.id
+            ] = exclusive_channel = await main_channel.category.create_voice_channel(
+                f"{member.name}的頻道", overwrites=overwrites
+            )
+            await member.move_to(exclusive_channel)
+            logging.info(
+                f"{member} create his/her exclusive channel(id={exclusive_channel.id})"
+            )
+
+    @InitedCog.listener()
+    async def on_ready(self):  # clear empty voice channel after restart
+        logging.info("Try to clear empty dynamic voice channel")
+        category: CategoryChannel = self.bot.get_channel(
+            self.config["main_category_id"]
+        )
+        if category:
+            for sub_channel in category.voice_channels:
+                if (
+                    sub_channel.id != self.config["main_channel_id"]
+                    and not sub_channel.members
+                ):
+                    await sub_channel.delete()
 
 
 def setup(bot: "RPMBot"):

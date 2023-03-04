f1df601c7e0b367cf9de77c0cf591d4a0bcaba52
Euxcbsks <hawhaw02030746@gmail.com>
2023-03-04 17:27:18 +0800

    feat: only import `RPMTWBot` on type checking

--- src/bot/core/extension.py
+++ src/bot/core/extension.py
@@ -1,14 +1,17 @@
 import logging
 from pathlib import Path
+from typing import TYPE_CHECKING
 
-from core.bot import RPMTWBot
 from discord import Cog
+
+if TYPE_CHECKING:
+    from core.bot import RPMTWBot
 
 extension_list = lambda: [i.stem for i in Path("./src/bot/extensions").glob("*.py")]
 
 
 class InitedCog(Cog):
-    def __init__(self, bot: RPMTWBot) -> None:
+    def __init__(self, bot: "RPMTWBot") -> None:
         super().__init__()
         self.bot = bot
         logging.debug(f"load extension - {self.__cog_name__}")

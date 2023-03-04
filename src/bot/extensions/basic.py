cf739316e1a67413c36eb7c43e626b02769e4693
Mango <evan940415@gmail.com>
2023-03-04 13:07:48 +0800

    feat: info command

--- src/bot/extensions/basic.py
+++ src/bot/extensions/basic.py
@@ -1,6 +1,8 @@
 from typing import TYPE_CHECKING
 from datetime import datetime
 
+from os import getpid
+from psutil import Process, virtual_memory
 from discord import Embed, Color
 
 from packages.cog_data import *
@@ -17,15 +19,26 @@ class BasicCog(InitedCog):
     @commands.slash_command()
     async def info(self, ctx: ApplicationContext):
         embed = Embed(color=Color.dark_gray())
-        embed.set_author(name=f'{self.bot.user}', icon_url=self.bot.user.avatar.url)
-        for name, value in zip(('正常運作時間', f'{(self.bot.online_time-datetime.now()).min}'),
-                               ('記憶體用量（目前使用量/常駐記憶體大小）', ''),
-                               ('使用者快取', f'{self.bot.users.__len__()}'),
-                               ('頻道快取', f'{self.bot.get_all_channels().__len__()}'),
-                               ('訊息快取', f'{self.bot.cached_messages}'),
-                               ('Shard 數量', f'{self.bot.shard_count}')):
-            embed.add_field(name=name, value=value)
-        await ctx.respond(embed = embed)
+        embed.set_author(name=f"{self.bot.user}", icon_url=self.bot.user.avatar.url)
+        for name, value, inline in (
+            (
+                "正常運作時間",
+                f"{round(((datetime.now()-self.bot.online_time).seconds)/60, 2)} 分鐘",
+                False,
+            ),
+            (
+                "記憶體用量（目前使用量/總記憶體大小）",
+                f"{round(Process(getpid()).memory_info().rss / 1024 ** 2, 2)} / {round(virtual_memory().total / 1024 ** 2, 2)} MiB",
+                False,
+            ),
+            ("使用者快取", f"{self.bot.users.__len__()}", True),
+            ("頻道快取", f"{[*self.bot.get_all_channels()].__len__()}", True),
+            ("訊息快取", f"{self.bot.cached_messages.__len__()}", True),
+            ("Shard 數量", f'{self.bot.shard_count or "0"}', False),
+        ):
+            print(name, value, inline)
+            embed.add_field(name=name, value=value, inline=inline)
+        await ctx.respond(embed=embed)
 
 
 def setup(bot: "RPMTWBot"):

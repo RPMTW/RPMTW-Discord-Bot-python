d0d98b074856c1ab7380a1797f62ffecdf6c491f
Euxcbsks <hawhaw02030746@gmail.com>
2023-03-06 11:41:17 +0800

    feat: `RPMTWBot` overwrite `run` method

--- src/bot/core/bot.py
+++ src/bot/core/bot.py
@@ -1,22 +1,21 @@
 import logging
 from datetime import datetime
-from os import environ, listdir
+from os import environ
 
 from core.extension import extension_list
 from core.universe_chat import RPMTWApiClient
 from discord import Bot, Intents
-from packages.default_data import Config
 from tomllib import load
 
 
 class RPMTWBot(Bot):
     def __init__(self):
+        self.online_time = datetime.now()
+
         with open("./constant.toml", "rb") as file:
             data = load(file)
             is_dev: bool = data["is_dev"]
             self.config: dict = data["test" if is_dev else "main"]
-
-        self.online_time = datetime.now()
 
         intents = Intents.default()
         intents.message_content = True
@@ -24,8 +23,12 @@ class RPMTWBot(Bot):
         super().__init__(intents=intents, **self.config["bot_settings"])
 
         self.rpmtw_api_client = RPMTWApiClient(self, self.config["UniverseChat"])
+        self.token = environ["TEST_TOKEN" if is_dev else "TOKEN"]
 
     async def on_ready(self):
         self.load_extensions(*(f"extensions.{file}" for file in extension_list()))
         await self.rpmtw_api_client.connect(environ.get("CHAT_TOKEN"))
         logging.info("bot is ready")
+
+    def run(self):
+        super().run(self.token)
--- src/bot/main.py
+++ src/bot/main.py
@@ -1,5 +1,3 @@
-from os import environ
-
 import dotenv
 from core.bot import RPMTWBot
 from packages.default_data import *
@@ -8,4 +6,4 @@ if __name__ == "__main__":
     dotenv.load_dotenv()
 
     bot = RPMTWBot()
-    bot.run(environ["TEST_TOKEN" if bot.test else "TOKEN"])
+    bot.run()

c3ccdf748925cce103cef21f3b7f82013efe5118
Euxcbsks <hawhaw02030746@gmail.com>
2023-03-07 11:46:14 +0800

    chore: add `debug_universe_chat_server.py`

---
+++ debug_universe_chat_server.py
@@ -0,0 +1,26 @@
+from uuid import uuid4
+
+from socketio import ASGIApp, AsyncServer
+from uvicorn import run
+
+sio = AsyncServer(async_mode="asgi")
+app = ASGIApp(sio)
+
+
+@sio.event
+def connect(sid, environ, auth):
+    print("connect ", sid)
+
+
+@sio.event
+def disconnect(sid):
+    print("disconnect ", sid)
+
+
+@sio.event
+def discordMessage(sid, data):
+    print(f"\nServer: {data}")
+    return str(uuid4())
+
+
+run(app, port=2087)

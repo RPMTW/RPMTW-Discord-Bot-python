37b02f4132431831be1c2eb9ab4f657472daac67
Ray <56812006+Euxcbsks@users.noreply.github.com>
2023-04-26 02:17:53 +0800

    Update event.py
    cache role

--- src/bot/extensions/event.py
+++ src/bot/extensions/event.py
@@ -7,8 +7,20 @@ from packages.cog_data import *
 
 if TYPE_CHECKING:
     from core.bot import RPMTWBot
+    from discord import Role
+
 
 class EventCog(InitedCog):
+    def get_role(self) -> "Role":
+        if _ := self._maybe_none.get("role"):
+            return _
+        
+        if not (_ := self.bot.get_guild(815819580840607807).get_role(945632168124751882)):
+            raise ValueError("Guild or role id invalid")
+        
+        self._maybe_none["role"] = _
+        return _
+            
     @InitedCog.listener()
     async def on_application_command_error(self, ctx: ApplicationContext, exc: DiscordException):
         if isinstance(exc, errors.NotOwner):
@@ -21,7 +33,7 @@ class EventCog(InitedCog):
         if message.author.bot:
             return
         if message.channel.id == 940533694697975849:
-            await message.author.add_roles(message.guild.get_role(945632168124751882))
+            await message.author.add_roles(self.get_role())
 
 def setup(bot: "RPMTWBot"):
     bot.add_cog(EventCog(bot))
